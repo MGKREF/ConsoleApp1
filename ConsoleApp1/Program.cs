cc8e012c8175ec56f35f1bb248d0defdbb7afd2c
S07C05 <S07C05@S07C05>
2023-10-18 09:11:20 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿class program
+{
+    class product
+
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+    static void Main()
+    {
+        product product = new product();
+    }
+}
